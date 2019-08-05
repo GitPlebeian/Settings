ff4f0d3e90e62e756133013f3079e1afbea051fa
Jackson Tubbs <decknot@Jacksons-MacBook-Pro.local>
2019-08-05 11:11:26 -0600

    Populating table view cell

--- Settings/Settings/View Controllers/Custom Cells/SettingTableViewCell.swift
+++ Settings/Settings/View Controllers/Custom Cells/SettingTableViewCell.swift
@@ -12,9 +12,30 @@ class SettingTableViewCell: UITableViewCell {
 
     // MARK: - Outlets
     
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var settingNameLabel: UILabel!
+    @IBOutlet weak var settingSwitch: UISwitch!
+    
     // MARK: - Properties
     
-    var setting: Setting?
+    var setting: Setting? {
+        didSet {
+            updateCellView()
+        }
+    }
     
     // MARK: - Custom Methods
+    
+    func updateCellView() {
+        if let setting = setting {
+            iconImageView.image = setting.icon
+            settingSwitch.isOn = setting.isOn
+            settingNameLabel.text = setting.name
+            
+            backgroundColor = setting.isOn ? .blue : .white
+        } else {
+            
+        }
+        
+    }
 }
