99f0ec2e72d1331b7f04b734bfbf11a70c133fc0
Jackson Tubbs <decknot@Jacksons-MacBook-Pro.local>
2019-08-05 10:29:36 -0600

    setting up settings model

---
+++ Settings/Settings/Models/Setting.swift
@@ -0,0 +1,22 @@
+//
+//  Setting.swift
+//  Settings
+//
+//  Created by Jackson Tubbs on 8/5/19.
+//  Copyright © 2019 Jax Tubbs. All rights reserved.
+//
+
+import UIKit
+
+class Setting {
+    
+    let name: String
+    let isOn: Bool
+    let icon: UIImage
+    
+    init(name: String, isOn: Bool = false, icon: UIImage) {
+        self.name = name
+        self.icon = icon
+        self.isOn = isOn
+    }
+}
